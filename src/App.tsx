af242dd4eaf8cc1dd2855a34815d3a4953da455f
Klender Carrasco <marcosklender@gmail.com>
2025-04-25 11:42:10 -0500

    Add Sass dependency and Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Button } from './components/shared/Button/Button';
 import './App.css'
 
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Botones</h1>
+      <Button variant="success">Success</Button>
+      <Button variant="danger">Danger</Button>
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
     </>
   )
 }
