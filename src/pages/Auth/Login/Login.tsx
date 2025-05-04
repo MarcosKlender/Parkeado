c050c8623908c477c66dfe79d65a3c2b3182a83c
Klender Carrasco <36539682+MarcosKlender@users.noreply.github.com>
2025-05-03 21:43:27 -0500

    Update Login and Register pages
    Add .env file to share the app name.

--- src/pages/Auth/Login/Login.tsx
+++ src/pages/Auth/Login/Login.tsx
@@ -1,22 +1,32 @@
+import reactLogo from "@/assets/react.svg"
 import { Input } from "@/components/shared/Input/Input"
 import { Button } from "@/components/shared/Button/Button"
+import './Login.scss'
 
 export function Login() {
+    const appName = import.meta.env.VITE_APP_NAME
+
     return (
-        <>
-            <h1>Login</h1>
-            <Input
-                label="Correo"
-                placeholder="usuario@ejemplo.com"
-                variant="email"
-            />
-            <Input
-                label="Contraseña"
-                placeholder="********"
-                variant="password"
-            />
-            <Button variant="success">Ingresar</Button>
-            <p>¿No tienes una cuenta? <a href="/register">Regístrate</a></p>
-        </>
+        <section className="login">
+            <img src={reactLogo} alt="Logo de React" width="50px" />
+            <h1>Inicio de Sesión</h1>
+            <p>Bienvenido a {appName}</p>
+            <hr />
+            <form>
+                <Input
+                    label="Correo"
+                    placeholder="usuario@ejemplo.com"
+                    variant="email"
+                />
+                <Input
+                    label="Contraseña"
+                    placeholder="********"
+                    variant="password"
+                />
+                <a href="/register">Olvidé mi contraseña</a>
+                <Button variant="success">Ingresar</Button>
+                <span>¿No tienes una cuenta? <a href="/register">Regístrate</a></span>
+            </form>
+        </section>
     )
 }
--- src/pages/Auth/Register/Register.tsx
+++ src/pages/Auth/Register/Register.tsx
@@ -1,7 +1,45 @@
+import reactLogo from "@/assets/react.svg"
+import { Input } from "@/components/shared/Input/Input"
+import { Button } from "@/components/shared/Button/Button"
+import './Register.scss'
+
 export function Register() {
     return (
-        <>
-            <h1>Register</h1>
-        </>
+        <section className="register">
+            <img src={reactLogo} alt="Logo de React" width="50px" />
+            <h1>Registro</h1>
+            <p>Vamos a crear tu nueva cuenta</p>
+            <hr />
+            <form>
+                <Input
+                    label="Nombre y Apellido"
+                    placeholder="John Doe"
+                    variant="text"
+                />
+                <Input
+                    label="Correo"
+                    placeholder="usuario@ejemplo.com"
+                    variant="email"
+                />
+                <Input
+                    label="Placa del Vehículo"
+                    placeholder="ABC-1234"
+                    variant="text"
+                />
+                <span>Podrás editar estos datos más adelante</span>
+                <Input
+                    label="Contraseña"
+                    placeholder="********"
+                    variant="password"
+                />
+                <Input
+                    label="Confirmar Contraseña"
+                    placeholder="********"
+                    variant="password"
+                />
+                <Button variant="success">Registrarse</Button>
+                <span>¿Ya tienes una cuenta? <a href="/">Inicia sesión</a></span>
+            </form>
+        </section>
     )
 }
