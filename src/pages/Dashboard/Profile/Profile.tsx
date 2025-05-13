d4f53f9163dddb982f031baa804342f985588e2a
Klender Carrasco <marcosklender@gmail.com>
2025-05-13 16:42:36 -0500

    Update Profile page

--- src/pages/Dashboard/Profile/Profile.tsx
+++ src/pages/Dashboard/Profile/Profile.tsx
@@ -1,5 +1,7 @@
 import { Header } from "@/components/shared/Header/Header"
 import { Hero } from "@/components/shared/Hero/Hero"
+import { Input } from "@/components/shared/Input/Input"
+import { Button } from "@/components/shared/Button/Button"
 import "./Profile.scss"
 
 export function Profile() {
@@ -11,6 +13,64 @@ export function Profile() {
                     title="Mi Perfil"
                     description="Aquí podrás editar la información de tu cuenta."
                 />
+                <div className="forms-container">
+                    <form>
+                        <h2>Datos del Usuario</h2>
+                        <Input
+                            label="Nombre y Apellido"
+                            id="name"
+                            name="name"
+                            placeholder="John Doe"
+                            variant="text"
+                            autoComplete="name"
+                        />
+                        <Input
+                            label="Correo"
+                            id="email"
+                            name="email"
+                            placeholder="usuario@example.com"
+                            variant="email"
+                            autoComplete="email"
+                        />
+                        <Input
+                            label="Placa del Vehículo"
+                            id="plate"
+                            name="plate"
+                            placeholder="ABC-1234"
+                            variant="text"
+                            autoComplete="on"
+                        />
+                        <Button type="submit" variant="success">Actualizar Datos</Button>
+                    </form>
+                    <form>
+                        <h2>Contraseña de Acceso</h2>
+                        <Input
+                            label="Contraseña Actual"
+                            id="current-password"
+                            name="current-password"
+                            placeholder="********"
+                            variant="password"
+                            autoComplete="current-password"
+                        />
+                        <Input
+                            label="Nueva Contraseña"
+                            id="new-password"
+                            name="new-password"
+                            placeholder="********"
+                            variant="password"
+                            autoComplete="new-password"
+                        />
+                        <Input
+                            label="Confirmar Nueva Contraseña"
+                            id="confirm-password"
+                            name="confirm-password"
+                            placeholder="********"
+                            variant="password"
+                            autoComplete="new-password"
+                        />
+                        <Button type="submit" variant="success">Cambiar Contraseña</Button>
+                    </form>
+                </div>
             </main>
         </>
     )
