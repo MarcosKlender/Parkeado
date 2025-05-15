3ef9625e0c56d7306db880696ebaeff3369f2b39
Klender Carrasco <marcosklender@gmail.com>
2025-05-15 11:38:53 -0500

    Update Reservation page

--- src/pages/Dashboard/Reservation/Reservation.tsx
+++ src/pages/Dashboard/Reservation/Reservation.tsx
@@ -1,6 +1,9 @@
 import { Header } from "@/components/shared/Header/Header";
 import { PageTitle } from "@/components/shared/PageTitle/PageTitle"
 import { HomeMap } from "@/components/maps/HomeMap/HomeMap";
+import { Parking } from "@/components/shared/Parking/Parking";
+import { ContentTitle } from "@/components/shared/ContentTitle/ContentTitle";
+import trafficIcon from "@/assets/traffic.svg"
 import "./Reservation.scss"
 
 export function Reservation() {
@@ -12,7 +15,17 @@ export function Reservation() {
                     title="Mi Reserva"
                     description="Aquí podrás administrar tu plaza de aparcamiento."
                 />
-                <HomeMap />
+                <div className="reservation-content">
+                    <HomeMap />
+                    <section>
+                        <ContentTitle
+                            icon={trafficIcon}
+                            title="Maneja con cuidado"
+                            description="Recuerda que tu reserva expirará automáticamente dentro de 1 hora."
+                        />
+                        <Parking name="Parqueadero 1" address="Avenida Siempreviva 742" availableSlots={20} />
+                    </section>
+                </div>
             </main>
         </>
     )
