ff8f7d0afa84366f2854580f742c032fc4cb0c56
Klender Carrasco <marcosklender@gmail.com>
2025-08-29 15:25:44 -0500

    Add Vitest and Testing Library setup
    Add setup file for jest-dom and happy-dom config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    setupFiles: "./tests/vitest.setup.ts",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
